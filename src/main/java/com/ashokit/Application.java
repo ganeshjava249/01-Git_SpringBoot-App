f0640bed9dcd0cf2e8884502f70cf4b5ebd071aa
Ganesh Satta <ganeshjava249@gmail.com>
2021-04-29 18:43:00 +0530

    HIS-133 debug fixed

--- src/main/java/com/ashokit/Application.java
+++ src/main/java/com/ashokit/Application.java
@@ -9,6 +9,7 @@ public class Application {
 
 	public static void main(String[] args) {
 		ConfigurableApplicationContext run = SpringApplication.run(Application.class, args);
+		//HIS-133 middle of another task like debug fixed done
 		run.close();
 	}
 
